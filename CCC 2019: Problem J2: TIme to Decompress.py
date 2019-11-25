99cb464ae8cd102b5d90f6244550a2f57a087a29
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-11-25 09:40:48 -0500

    Create CCC 2019: Problem J2: TIme to Decompress.py

---
+++ CCC 2019: Problem J2: TIme to Decompress.py
@@ -0,0 +1,8 @@
+code = []
+
+for i in range(int(input())): code.append(input())
+
+for i in code:
+  values = i.split()
+  for x in range(int(values[0])-1): print(values[1], end = "")
+  print(values[1])
