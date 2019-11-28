af3ab2647610eac104b522c2df0124c08d710c28
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-11-28 06:52:58 -0500

    Create Problem J4: Flipper.py

---
+++ 2019/Problem J4: Flipper.py
@@ -0,0 +1,9 @@
+rule = list(input())
+
+grid = [1, 2, 3, 4]
+
+if rule.count("H") % 2 == 1: grid = [3, 4, 1, 2]
+if rule.count("V") % 2 == 1: grid = [grid[1], grid[0], grid[3], grid[2]]
+
+print(grid[0], grid[1])
+print(grid[2], grid[3])
