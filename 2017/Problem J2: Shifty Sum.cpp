5db43e178182540ac40c98549b55576c0af17739
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-07 10:07:16 -0500

    Create Problem J2: Shifty Sum.cpp

---
+++ 2017/Problem J2: Shifty Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int numb; int rep; cin >> numb >> rep;
+  int var = numb; int total = numb;
+  
+  for (int i = 1;  i < rep+1; i++) {
+    var *= 10;
+    total += var;
+  }
+
+  cout << total;
+}
