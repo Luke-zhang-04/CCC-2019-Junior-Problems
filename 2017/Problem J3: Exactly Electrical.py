122264103c27cc380c0f51b896618dc0486b9b16
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-12 21:21:57 -0500

    Create Problem J3: Exactly Electrical.py

---
+++ 2017/Problem J3: Exactly Electrical.py
@@ -0,0 +1,9 @@
+start = list(map(int, input().split(" ")))
+end = list(map(int, input().split(" ")))
+charge = int(input())
+
+distance = abs((end[1] - start[1] ) + (end[0] - start[0]))
+
+print(distance)
+
+print('Y' if charge >= distance and (charge - distance) % 2 == 0 else 'N')
