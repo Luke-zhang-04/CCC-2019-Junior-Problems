855bb8cf55a686058309493c83d4b8b067e40d16
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-13 13:20:54 -0500

    Update Problem J1: Quadrant Selection.py

--- 2017/Problem J1: Quadrant Selection.py
+++ 2017/Problem J1: Quadrant Selection.py
@@ -1,7 +1,14 @@
 def quadrant(x, y):
-  if x > 0 and y > 0: return 1
-  elif x < 0 and y > 0: return 2
-  elif x < 0 and y < 0: return 3
-  elif x > 0 and y < 0: return 4
+  if x > 0 and y > 0:
+    return 1
+  
+  elif x < 0 and y > 0:
+    return 2
+  
+  elif x < 0 and y < 0:
+    return 3
+  
+  elif x > 0 and y < 0:
+    return 4
 
 print(quadrant(int(input()), int(input())))
