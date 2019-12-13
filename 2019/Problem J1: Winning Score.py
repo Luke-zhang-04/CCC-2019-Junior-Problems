f56220b61ff00c7338b4124804c3e0a2b6d6f709
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-13 13:14:34 -0500

    Update Problem J1: Winning Score.py

--- 2019/Problem J1: Winning Score.py
+++ 2019/Problem J1: Winning Score.py
@@ -1,10 +1,10 @@
-score = lambda a, b, c: 3*a+2*b+c
+def score(a, b, c): return 3*a+2*b+c
 
 aScore = score(int(input()), int(input()), int(input()))
 bScore = score(int(input()), int(input()), int(input()))
 
-winner = 'A' if aScore > bScore else 'B'
-
 if aScore == bScore: winner = 'T'
 
+winner = 'A' if aScore > bScore else 'B'
+  
 print(winner)
