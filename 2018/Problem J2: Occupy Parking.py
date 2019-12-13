a97a6659fb416deb7bac16dd7f042987c0f4c46f
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-13 13:19:00 -0500

    Conforms to PEP8 sytle guide

--- 2018/Problem J2: Occupy Parking.py
+++ 2018/Problem J2: Occupy Parking.py
@@ -3,6 +3,7 @@ spaces = int(input())
 day1 = list(input())
 day2 = list(input())
 
-for i in range(spaces): amt += 1 if day1[i] == "C" and day2[i] == 'C' else 0
+for i in range(spaces):
+  amt += 1 if day1[i] == "C" and day2[i] == 'C' else 0
 
 print(amt)
