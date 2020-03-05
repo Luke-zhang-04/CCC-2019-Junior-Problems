9dca419dfab8829af3331e739e20e8003f2a6eb2
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-05 07:31:40 -0500

    added flipper.cpp

---
+++ 2019/Problem J4: Flipper.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int count(std::vector<char> arr, char target) {
+    int total = 0;
+    for (const char &i: arr) {
+        if (i == target) {
+            total ++;
+        }
+    }
+    return total;
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+    std::vector<char> rule;
+
+    for (const char &i: input) {
+        rule.push_back(i);
+    }
+
+    std::vector<int> grid = {1, 2, 3, 4};
+
+    if (count(rule, 'H') % 2 == 1) {
+        grid = {3, 4, 1, 2};
+    }
+    if (count(rule, 'V') % 2 == 1) {
+        grid = {grid[1], grid[0], grid[3], grid[2]};
+    }
+
+    cout << grid[0] << grid[1] << endl << grid[2] << grid[3] << endl;
+}
