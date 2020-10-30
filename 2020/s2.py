bf27f1d2829b22f18f662900225ee509940d2ee7
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-10-30 15:33:58 -0400

    partial commit for 2020/s2
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- 2020/s2.py
+++ 2020/s2.py
@@ -8,9 +8,26 @@ squares = {}
 for i in range(rows):
     vals = input().split(" ")
 
-    for (index, val) in enumerate(vals):
-        squares[f"{i}, {index}"] = int(val)
+    for index, val in enumerate(vals):
+        squares[f"{i} {index}"] = int(val)
 
-print(squares)
+class Node:
+    children = []
 
+    def __init__ (self, coords, val):
+        self.coords = coords
+        self.val = val
+
+        x, y = coords.split(" ")
+        self.multiplied = int(x) * int(y)
+
+    def traverse (self):
+        append = self.children.append
+        multiplied = self.multiplied
+
+        for square in squares:
+            if (multiplied == squares[square]):
+                append(Node(square, squares[square]))
+
+        map(lambda child: child.traverse(), self.children)
 
