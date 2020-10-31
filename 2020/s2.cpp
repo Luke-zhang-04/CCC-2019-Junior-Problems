dbe9553829e97fc169d63b7d0b70250a9ea384c7
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-10-31 16:10:15 -0400

    solution(2020/s2): finalize solution
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- 2020/s2.cpp
+++ 2020/s2.cpp
@@ -14,11 +14,26 @@ using std::vector;
 #define set std::unordered_set
 #define map std::unordered_map
 
-uint rows;
-uint columns;
+uint rows;    // No. of rows
+uint columns; // No. of columns
+
+/**
+ * Visited squares.
+ * We only need one visited set because if the square has been visited we know
+ * there's no way that square will take us to the end.
+ * This way, we don't look through the same path multiple times.
+ */
 set<uint> visited;
+
+/**
+ * Squares to be stored in the following way:
+ * {valueOfSquare: [productOfCoords...]}
+ */
 map<uint, vector<uint>> squares;
 
+/**
+ * Splits str by spaces and returns a vector
+ */
 vector<string> split(string str) {
     vector<string> words;
     std::istringstream ss(str);
@@ -31,17 +46,25 @@ vector<string> split(string str) {
     return words;
 }
 
+/**
+ * DFS resurcive function to solve problem
+ */
 bool solve(uint squareValue) {
+    // If the value of the square is 1, it can only go to(1, 1).
+    // Therefore, we have reached the end.
     if (squareValue == 1) {
         return true;
-    } else if (squares.find(squareValue) == squares.end()) {
+    } else if (squares.find(squareValue) == squares.end()) { // If square with
+                                                             // value doesn't
+                                                             // exist, return
         return false;
     }
 
     for (auto& index : squares.at(squareValue)) {
-        if (visited.find(index) == visited.end()) {
-            visited.insert(index);
-            if (solve(index)) {
+        if (visited.find(index) ==
+            visited.end()) {       // If square hasn't been visited
+            visited.insert(index); // Add to visited
+            if (solve(index)) {    // Solve
                 return true;
             }
         }
@@ -52,14 +75,15 @@ bool solve(uint squareValue) {
 
 int main() {
     cin >> rows >> columns;
-    cin.ignore();
+    cin.ignore(); // Ignore newline
 
     for (uint i = 0; i < rows; i++) {
         string vals;
         std::getline(cin, vals);
 
-        auto splitVals = split(vals);
+        auto splitVals = split(vals); // Split input by space
 
+        // Add squares to the squares map
         for (uint index = 0; index < splitVals.size(); index++) {
             uint val = std::stoi(splitVals[index]);
             uint squareValue = (i + 1) * (index + 1);
