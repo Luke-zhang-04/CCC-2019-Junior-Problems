17f3ba7ba5e0310c9d42e159c4161981329e133a
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-10-31 11:57:06 -0400

    solution(2020/s1): add comments and format code
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- 2020/s1.py
+++ 2020/s1.py
@@ -1,21 +1,24 @@
 #!/bin/python3
 
-entryCount = int(input())
-entries = []
-fastestSpeed = 0
+entryCount = int(input())  # No of observations
+entries = []  # All observations
+fastestSpeed = 0  # Fastest recorded speed
 
 
-for entry in range(entryCount):
-    (time, speed) = input().split(" ")
+for _ in range(entryCount):  # For number of entries, add the entry to entries[]
+    time, speed = input().split(" ")
     entries.append((int(time), int(speed)))
 
-entries.sort()
+entries.sort()  # Sort the entries. Python will sort by first element in the tuple
 
+# Go through each sprint time
 for entry in range(0, entryCount - 1, 1):
-    distance = entries[entry + 1][1] - entries[entry][1]
-    time = entries[entry + 1][0] - entries[entry][0]
-    prevSpeed = abs(distance) / time
+    distance = entries[entry + 1][1] - entries[entry][1]  # Distance covered
+    time = entries[entry + 1][0] - entries[entry][0]  # Delta time
+    prevSpeed = abs(distance) / time  # v = d/t
 
-    fastestSpeed = max(fastestSpeed, prevSpeed)
+    fastestSpeed = max(
+        fastestSpeed, prevSpeed
+    )  # If this speed is faster, add it. Othersise, use old speed
 
 print(fastestSpeed)
