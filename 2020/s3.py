338cca4fc8e8cb75241282a132ecb4ed879a7d4f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-11-02 18:36:51 -0500

    solution(2020/s3): add python solution
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- 2020/s3.py
+++ 2020/s3.py
@@ -1,35 +1,70 @@
 #!/bin/python3
+# NOTE: This solution gives 7/15 points. On test 115, it times out @3.2 seconds.
 
 import sys
 
+# Get needle and haystack strings
 needle = sys.stdin.readline()
 haystack = sys.stdin.readline()
 
+needleLength = len(needle)  # Get needle length now so we don't call len all the time
 
-def getLetters(string):
-    letters = {}
+"""Dict of letters and how many times they appear in the needle
+E.g aab will be {"a": 2, "b": 1}
+"""
+needleLetters = {}
 
-    for letter in string:
-        letters[letter] = letters.setdefault(letter, 0) + 1
+foundPermutations = set()  # Set of found permutations, to be hashed
+prevLetters = {}  # The same as needleLetters, except for each part of the haystack
+prevLetter = None  # The last letter of the previous portion of the haystack
 
-    letters.pop("\n", None)
+add = foundPermutations.add  # Keep the set.add function in a variable for speed
 
-    return letters
+for letter in needle:  # For each letter in needle, add it to needleLetters
+    needleLetters[letter] = needleLetters.setdefault(letter, 0) + 1
 
+# Remove the \n stat which is a side effect of sys.stdin.readline()
+needleLetters.pop("\n", None)
 
-needleLength = len(needle)
-needleLetters = getLetters(needle)
-foundPermutations = set()
-add = foundPermutations.add
-
+# "Sliding window" algorithm, with a fixed window size
 for index in range(0, len(haystack) - needleLength + 1):
+    # Get the letters within the window
     needleSlice = haystack[index : index + needleLength - 1]
 
-    if (
-        getLetters(needleSlice) == needleLetters
-        and needleSlice not in foundPermutations
-    ):
-        add(needleSlice)
+    """If no previous letter is found, perform the same loop as performed above.
 
+    Otherwise, we can use the previous data. We can subtract 1 from the previous
+    window's first letter, and add 1 to the current windows last letter. This way,
+    we don't needlessly loop through needleSlice again.
+    """
+    if prevLetter == None:
+        for letter in needleSlice:
+            prevLetters[letter] = prevLetters.setdefault(letter, 0) + 1
+    else:
+        # Previous windows first letter
+        removedLetterValue = prevLetters[prevLetter] - 1
+
+        # If the subtracted value ends up as 0, just delete it
+        if removedLetterValue == 0:
+            del prevLetters[prevLetter]
+        else:
+            prevLetters[prevLetter] = removedLetterValue
+
+        # Add the last letter to prevLetters
+        prevLetters[needleSlice[-1]] = prevLetters.setdefault(needleSlice[-1], 0) + 1
+
+    # Assign prevLetter to the first letter of the current window
+    prevLetter = needleSlice[0]
+
+    """We need to use a hashing algorithm, because after test 114, the strings
+    become too large, and we get a memory error in the grader.
+
+    We run hash(needleSlice) twice instead of storing a variable because storing
+    it in a variable causes it to run SLOWER than if it were called twice.
+    """
+
+    # If the two dictionaries are the same, and the hashed slice isn't already found
+    if prevLetters == needleLetters and hash(needleSlice) not in foundPermutations:
+        add(hash(needleSlice))  # Use the foundPermutations.add function
 
 print(len(foundPermutations))
