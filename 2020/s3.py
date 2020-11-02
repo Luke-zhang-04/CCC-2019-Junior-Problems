179db80691a5b5b4c6ca3b9c4aae4b7d11327e19
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-11-02 11:19:15 -0500

    solution(2020/s3): finish unoptimized solution
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ 2020/s3.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import sys
+
+needle = sys.stdin.readline()
+haystack = sys.stdin.readline()
+
+
+def getLetters(string):
+    letters = {}
+
+    for letter in string:
+        letters[letter] = letters.setdefault(letter, 0) + 1
+
+    letters.pop("\n", None)
+
+    return letters
+
+
+needleLength = len(needle)
+needleLetters = getLetters(needle)
+foundPermutations = set()
+add = foundPermutations.add
+
+for index in range(0, len(haystack) - needleLength + 1):
+    needleSlice = haystack[index:index + needleLength - 1]
+
+    if (
+        getLetters(needleSlice) == needleLetters
+        and needleSlice not in foundPermutations
+    ):
+        add(needleSlice)
+
+
+print(len(foundPermutations))
