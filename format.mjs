8425d1796fc755e4c116c341ea95ab9da584e185
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-01-18 20:29:37 -0500

    update formatter
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- format.mjs
+++ format.mjs
@@ -10,12 +10,20 @@
 import chokidar from "chokidar"
 import childProcess from "child_process"
 
+let start = Date.now()
 
 const resolveChildProcess = (err, stdout, stderr) => {
     err && console.error(err)
     stderr && console.error(stderr)
     stdout && console.log(stdout)
+
+    if (!err && !stderr && !stdout) {
+        console.log("All done!\n")
+    }
+
+    console.log(`Done in ${Date.now() - start}ms`)
 }
+
 
 childProcess.exec(
     "clang-format **/*.cpp -style=file -i &\n black **/*.py &\n wait",
@@ -31,14 +39,20 @@ if (process.argv.includes("--watch") || process.argv.includes("-w")) {
             atomic: 1250,
         },
     ).on("change", async (path) => {
-        console.log("Formatting")
-
         if (path.includes(".cpp")) {
+            console.clear()
+            console.log("Formatting . . .\n")
+            start = Date.now()
+
             childProcess.exec(
                 `clang-format ${path} -style=file -i`,
                 resolveChildProcess,
             )
         } else if (path.includes(".py")) {
+            console.clear()
+            console.log("Formatting . . .\n")
+            start = Date.now()
+
             childProcess.exec(`black ${path}`, resolveChildProcess)
         }
     })
